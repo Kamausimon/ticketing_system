c57d27876069b835148f997afb1d69f457a9350b
kamau simon <kamausimon217@gmail.com>
2025-11-12 00:11:25 +0300

    add TicketOrder struct to manage ticket orders with foreign key relationships

--- internal/models/ticketsOrder.go
+++ internal/models/ticketsOrder.go
@@ -1,1 +1,11 @@
 package models
+
+import "gorm.io/gorm"
+
+type TicketOrder struct {
+	gorm.Model
+	OrderID  uint   `gorm:"not null;index"`
+	Order    Order  `gorm:"foreignKey:OrderID"`
+	TicketID uint   `gorm:"not null;index"`
+	Ticket   Ticket `gorm:"foreignKey:TicketID"`
+}
