9a9fe9f2039cbb4a0a78305ce75afbd05ca31cab
kamau simon <kamausimon217@gmail.com>
2025-11-12 00:10:28 +0300

    add OrderItem struct with fields for order details and ticket information

--- internal/models/orderItems.go
+++ internal/models/orderItems.go
@@ -1,1 +1,17 @@
 package models
+
+import "gorm.io/gorm"
+
+type OrderItem struct {
+	gorm.Model
+	OrderID          uint        `gorm:"not null;index"`
+	Order            Order       `gorm:"foreignKey:OrderID"`
+	TicketClassID    uint        `gorm:"not null;index"`
+	TicketClass      TicketClass `gorm:"foreignKey:TicketClassID"`
+	Quantity         int         `gorm:"not null"`
+	UnitPrice        float32     `gorm:"not null"`
+	TotalPrice       float32     `gorm:"not null"`
+	Discount         *float32
+	PromoCodeUsed    *string
+	GeneratedTickets []Ticket `gorm:"foreignKey:OrderItemID"`
+}
