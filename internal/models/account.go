7270f94e282a1bae93bb99d59875453816713ab8
kamau simon <kamausimon217@gmail.com>
2025-11-15 17:27:16 +0300

    add Account struct with fields for user account management and payment gateway integration

--- internal/models/account.go
+++ internal/models/account.go
@@ -1,4 +1,34 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Account struct {
+	gorm.Model
+	FirstName            string
+	LastName             string
+	Email                string
+	TimezoneID           *int
+	DateFormatID         *int
+	DateTimeFormatID     *int
+	CurrencyID           *int
+	LastIP               *string
+	LastLoginDate        *time.Time
+	Address1             *string
+	Address2             *string
+	City                 *string
+	County               *string
+	PostalCode           *string
+	IsActive             bool `gorm:"default:true"`
+	IsBanned             bool `gorm:"default:false"`
+	StripeAccessToken    *string
+	StripeRefreshToken   *string
+	StripeSecretKey      *string
+	StripePublishableKey *string
+	StripeDataRaw        *string
+	PaymentGatewayID     uint
+	PaymentGateway       PaymentGateway
 }
