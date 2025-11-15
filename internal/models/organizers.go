284f92f35274a0a195390e5999ddb5a2c91b9a65
kamau simon <kamausimon217@gmail.com>
2025-11-15 17:28:24 +0300

    add Organizer struct to manage organizer details and relationships with accounts

--- internal/models/organizers.go
+++ internal/models/organizers.go
@@ -1,4 +1,28 @@
 package models
 
+import "gorm.io/gorm"
+
 type Organizer struct {
+	gorm.Model
+	AccountID           uint    `gorm:"not null;index"`
+	Account             Account `gorm:"foreignKey:AccountID"`
+	Name                string
+	About               string
+	Email               string
+	Phone               string
+	ConfirmationKey     string
+	Facebook            string
+	Twitter             string
+	LogoPath            *string
+	IsEmailConfirmed    bool `gorm:"default:false"`
+	ShowTwitterWidget   bool
+	ShowFacebookWidget  bool
+	TaxName             string
+	TaxValue            float32
+	TaxPin              string
+	ChargeTax           int
+	PageHeaderBgColor   string
+	PageBgColor         string
+	PageTextColor       string
+	EnableOrganizerPage bool
 }
