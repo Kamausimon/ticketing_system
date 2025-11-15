8870025d11100244631c83fb03db33dfbe2d9956
kamau simon <kamausimon217@gmail.com>
2025-11-15 17:28:14 +0300

    add EventImages struct to manage event image data with relationships to Event, Account, and User

--- internal/models/eventImages.go
+++ internal/models/eventImages.go
@@ -1,4 +1,14 @@
 package models
 
+import "gorm.io/gorm"
+
 type EventImages struct {
+	gorm.Model
+	ImagePath string
+	EventID   uint    `gorm:"not null;index"`
+	Event     Event   `gorm:"foreignKey:EventID"`
+	AccountID uint    `gorm:"not null;index"`
+	Account   Account `gorm:"foreignKey:AccountID"`
+	UserID    uint    `gorm:"not null;index"`
+	User      User    `gorm:"foreignKey:UserID"`
 }
