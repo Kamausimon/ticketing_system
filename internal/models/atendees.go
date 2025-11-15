4e655447c63b14029363097e4f729ea933354c1d
kamau simon <kamausimon217@gmail.com>
2025-11-15 17:27:36 +0300

    add Attendee struct to manage event attendees with relevant fields and relationships

--- internal/models/atendees.go
+++ internal/models/atendees.go
@@ -1,1 +1,26 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Attendee struct {
+	gorm.Model
+	OrderID                uint   `gorm:"not null;index"`
+	Order                  Order  `gorm:"foreignKey:OrderID"`
+	EventID                uint   `gorm:"not null;index"`
+	Event                  Event  `gorm:"foreignKey:EventID"`
+	TicketID               uint   `gorm:"not null;index"`
+	Ticket                 Ticket `gorm:"foreignKey:TicketID"`
+	FirstName              string
+	LastName               string
+	Email                  string
+	HasArrived             bool
+	ArrivalTime            *time.Time
+	AccountID              uint    `gorm:"not null;index"`
+	Account                Account `gorm:"foreignKey:AccountID"`
+	IsRefunded             bool    `gorm:"default:false"`
+	PrivateReferenceNumber int
+}
