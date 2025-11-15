9974dc5263d4a767c1d93c9f83bb119efb84e213
kamau simon <kamausimon217@gmail.com>
2025-11-15 17:29:48 +0300

    add ReservedTicket model with fields for ticket and event associations, quantity reserved, expiration, and session ID

--- internal/models/reservedTickets.go
+++ internal/models/reservedTickets.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type ReservedTicket struct {
+	gorm.Model
+	TicketID         uint   `gorm:"not null;index"`
+	Ticket           Ticket `gorm:"foreignKey:TicketID"`
+	EventID          uint   `gorm:"not null;index"`
+	Event            Event  `gorm:"foreignKey:EventID"`
+	QuantityReserved int
+	Expires          time.Time
+	SessionID        string
+}
