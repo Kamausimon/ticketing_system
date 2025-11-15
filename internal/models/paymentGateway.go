19739df749810ab5a934d116b2109abfdd12602a
kamau simon <kamausimon217@gmail.com>
2025-11-15 17:28:49 +0300

    add PaymentGateway model with fields for provider details and refund capabilities

--- internal/models/paymentGateway.go
+++ internal/models/paymentGateway.go
@@ -1,4 +1,12 @@
 package models
 
+import "gorm.io/gorm"
+
 type PaymentGateway struct {
+	gorm.Model
+	ProviderName string
+	ProviderURL  string
+	IsOnSite     bool
+	CanRefund    bool
+	Name         string
 }
