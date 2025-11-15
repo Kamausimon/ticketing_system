13e15db723126b0aa85efd7a046d7dbb7a04baa7
kamau simon <kamausimon217@gmail.com>
2025-11-15 20:27:00 +0300

    refactor: replace float32 with Money type for price fields in OrderItem and TicketClass models

--- internal/models/orderItems.go
+++ internal/models/orderItems.go
@@ -9,9 +9,9 @@ type OrderItem struct {
 	TicketClassID    uint        `gorm:"not null;index"`
 	TicketClass      TicketClass `gorm:"foreignKey:TicketClassID"`
 	Quantity         int         `gorm:"not null"`
-	UnitPrice        float32     `gorm:"not null"`
-	TotalPrice       float32     `gorm:"not null"`
-	Discount         *float32
+	UnitPrice        Money       `gorm:"not null"` // Use Money type instead of float32
+	TotalPrice       Money       `gorm:"not null"` // Use Money type instead of float32
+	Discount         *Money      // Use Money type instead of float32
 	PromoCodeUsed    *string
 	GeneratedTickets []Ticket `gorm:"foreignKey:OrderItemID"`
 }
--- internal/models/ticketsClasses.go
+++ internal/models/ticketsClasses.go
@@ -12,18 +12,18 @@ type TicketClass struct {
 	Event               Event  `gorm:"foreignKey:EventID"`
 	Name                string `gorm:"not null"`
 	Description         string
-	Price               float32 `gorm:"not null"`
-	Currency            string  `gorm:"not null;default:'KSH'"`
+	Price               Money  `gorm:"not null"` // Use Money type instead of float32
+	Currency            string `gorm:"not null;default:'KSH'"`
 	MaxPerOrder         *int
 	MinPerOrder         *int `gorm:"default:1"`
 	QuantityAvailable   *int
 	QuantitySold        int `gorm:"default:0"`
 	StartSaleDate       *time.Time
 	EndSaleDate         *time.Time
-	SalesVolume         float32
-	OrganizerFeesVolume float32
-	IsPaused            bool `gorm:"default:false"`
-	IsHidden            bool `gorm:"default:false"`
-	SortOrder           int  `gorm:"default:0"`
-	RequiresApproval    bool `gorm:"default:false"`
+	SalesVolume         Money `gorm:"default:0"` // Use Money type instead of float32
+	OrganizerFeesVolume Money `gorm:"default:0"` // Use Money type instead of float32
+	IsPaused            bool  `gorm:"default:false"`
+	IsHidden            bool  `gorm:"default:false"`
+	SortOrder           int   `gorm:"default:0"`
+	RequiresApproval    bool  `gorm:"default:false"`
 }
